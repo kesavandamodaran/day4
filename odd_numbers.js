90a607263edb4eb3af0fc9acbfea94fa614df9fa
kesavandamodaran <159546396+kesavandamodaran@users.noreply.github.com>
2024-02-22 14:47:33 +0530

    Add files via upload

---
+++ odd_numbers.js
@@ -0,0 +1,22 @@
+// 1. Print odd numbers in an array:
+
+// Anonymous Function
+const printOddNumbers = function(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 !== 0) {
+            console.log(arr[i]);
+        }
+    }
+}
+
+printOddNumbers([1, 2, 3, 4, 5]);
+
+// IIFE
+(function(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 !== 0) {
+            console.log(arr[i]);
+        }
+    }
+})([1, 2, 3, 4, 5]);
+
